deaef1bc07d1066b28ab8223ab6a0a158efe8735
Lance Clifner <clifn100@mail.chapman.edu>
2016-10-17 23:03:03 +0000

    rename testnotebook to correct name fix julia function to return correct values for all cases add unit test functions for juliaplane

--- cplane_np.py
+++ cplane_np.py
@@ -2,24 +2,26 @@
 
 import numpy as np
 import pandas as pd
+import csv                # for I/O with csv format files
+import json               # for I/O with json formatted data
 import abscplane as absc
 import matplotlib.pyplot as plt
 
-"""This is the Class ComplexPlaneNP.  It is built from the Abstract Class AbsComplexPlane.
-This Class serves as a simplistic pan/zoom over a 2D complex plane, where each point in the
-plane undergoes a transformation through the function f(), where the value at the coordinate
-point is:
-    value = f( x + yj )
-
-This class uses the data structures available in numpy and pandas.  The array data structure
-is used from numpy to set up the rows for the plane.  The rows are then inserted into a pandas
-DataFrame and given row and column names to help identify the rows and columns in the plane.
-
-The contents of each 'cell' in the ComplexPlaneNP is of type imaginary number.
-"""
 class ComplexPlaneNP(absc.AbsComplexPlane):
+    """This is the Class ComplexPlaneNP.  It is built from the Abstract Class AbsComplexPlane.
+    This Class serves as a simplistic pan/zoom over a 2D complex plane, where each point in the
+    plane undergoes a transformation through the function f(), where the value at the coordinate
+    point is:
+        value = f( x + yj )
 
-    def __init__(self, newXmin=-5., newXmax=5., newYmin=-5., newYmax=5., f=lambda x: x):
+    This class uses the data structures available in numpy and pandas.  The array data structure
+    is used from numpy to set up the rows for the plane.  The rows are then inserted into a pandas
+    DataFrame and given row and column names to help identify the rows and columns in the plane.
+
+    The contents of each 'cell' in the ComplexPlaneNP is of type imaginary number.
+    """
+
+    def __init__(self, newXmin=-5., newXmax=5., newYmin=-5., newYmax=5., f=lambda x: x, maxLoop=100):
         """This is the creator.  It can be passed the the min/max X and Y values for the plane,
         and a transformation function (f).  There are default values if the parameters are not
         passed to the creator.
@@ -39,6 +41,7 @@ class ComplexPlaneNP(absc.AbsComplexPlane):
         self.xstep = (self.xmax - self.xmin)/(self.xlen - 1)
         self.ystep = (self.ymax - self.ymin)/(self.ylen - 1)
         self.f = f
+        self.max = maxLoop
         #  call refresh() to generate the the plane and its contents
         self.refresh()
 
@@ -84,191 +87,197 @@ class ComplexPlaneNP(absc.AbsComplexPlane):
 
 
 class JuliaPlane(ComplexPlaneNP):
+    """This is the Class JuliaPlane.  It is built from the Class ComplexPlaneNP.
+    This Class serves as a simplistic pan/zoom over a 2D complex plane, where each point in the
+    plane undergoes a transformation through a function f() created by a call to the function julia().
+    This transformation function returns an integer, which is stored in the JuliaPlane.
+    NOTE that the julia function is included in this source file, but is not part of the JuliaPlane()
+    class.
 
-    """NEED TO ADD IN THE STUFF FROM THE HOMEWORK"""
+    The contents of each 'cell' in the JuliaPlane is of type integer.
+    """
 
-    def __init__(self, newXmin=-5., newXmax=5., newYmin=-5., newYmax=5., c=(-1.037 + 0.17j), max=100):
-        ComplexPlaneNP.__init__(self, newXmin, newXmax, newYmin, newYmax)
-        self.set_f(c, max)
+    def __init__(self, newXmin=-5., newXmax=5., newYmin=-5., newYmax=5., c=(-1.037 + 0.17j), maxLoop=100):
+        """ The JuliaPlane creator method uses the ComplexPlaneNP creator to generate the initial 2D plane.
+        The function for this plane is then reset to a new function, and the values re-generated.
+        Note that since this function was intially created, the f parameter was added to ComplexPlaneNP's
+        creator, but this code was not updated to take advantage of the passed parameter change.
+        """
+        #  set the function and re-compute the plane's values
+        f = julia(c, maxLoop)
+        self.c = c
+        ComplexPlaneNP.__init__(self, newXmin, newXmax, newYmin, newYmax, f, maxLoop)
 
     def show(self, chosenmap=plt.cm.hot):
+        """This method plots an image of the contents of the 2D complex plane.  The numbers in the plane
+        are treated as gray-scale in matplotlib.imshow(), with an optional color map being used to turn
+        the gray-scale into various color combinations.
+        """
         plt.clf()
         plt.imshow(abs(self.plane.as_matrix()), cmap=chosenmap, interpolation='bicubic', extent=(self.xmin, self.xmax, self.ymin, self.ymax))
         plt.show()
 
     def set_f(self, c, max=100):
+        """This method is used to set the transformation function in the ComplexPlane for this JuliaPlane.
+        The function julia is currently not a member of JuliaPlane.
+        This method sets the complex value 'c' in the julia function and defaults to a maximum iteration count
+        of 100.
+        """
+        self.c = c  # keep a copy for the CSV and JSON output
         self.f = julia(c, max)
         self.refresh()
 
 
+    def toCSV( self, filename ):
+        """Output the contents of the julia plane and all parameters needed to recreate it in CSV format
+        Created with the help of https://docs.python.org/3/library/csv.html
+        """
+        #  open the file for writing
+        with open(filename, 'w', newline='' ) as csvfile:
+            writer = csv.writer( csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL )
+
+            # write out the parameters needed to rebuild the julia plane
+            writer.writerow( [ 'JuliaPlane', 'xmin', 'xmax', 'xlen', 'ymin', 'ymax', 'ylen', 'c' ] )
+            writer.writerow( [ '', self.xmin, self.xmax, self.xlen, self.ymin, self.ymax, self.ylen, self.c ] )
+            # blank row
+            writer.writerow( [ 'JuliaPlane Contents' ] )
+            #  write out the contents of the plane
+            for row_i in self.plane.iterrows():
+                writer.writerows( row_i[1:self.xlen] )
+
+            #  we're done, clean up the file
+            csvfile.close()
+
+
+    def fromCSV( self, filename ):
+        """Read in the contents of a csv file to rebuild a save julia plane.  We are only interested in
+        the parameters necessary to reconstruct the plane, we do not need to read the contents of the plane
+        itself"""
+        #  open the file for reading
+        with open( filename, newline='' ) as csvfile:
+            reader = csv.reader( csvfile, delimiter=',', quotechar='"' )
+            count = 1
+            for row in reader:
+                if count == 2:
+                    #  parse the second row only
+                    self.xmin  = float( row[ 1 ] )
+                    self.xmax  = float( row[ 2 ] )
+                    self.xlen  = int( row[ 3 ] )
+                    self.ymin  = float( row[ 4 ] )
+                    self.ymax  = float( row[ 5 ] )
+                    self.ylen  = int( row[ 6 ] )
+                    self.c     = complex( row[ 7 ] )
+                    self.xstep = (self.xmax - self.xmin)/(self.xlen - 1)
+                    self.ystep = (self.ymax - self.ymin)/(self.ylen - 1)
+                    self.set_f( self.c )      # set the tranformation function.  Note that this automatically calls refresh()
+                    break
+                elif count == 1:
+                    #  this is a header row, which we can skip
+                    count += 1
+                else:
+                    # discard all subsequent rows
+                    break
+
+            #  we're done, clean up the file
+            csvfile.close()
+
+
+    def toJSON( self, filename ):
+        """Output the contents of the julia plane and all parameters needed to recreate it in JSON encoded format
+        Created with the help of http://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file-in-python
+        """
+        #  open the file for writing
+        with open( filename, 'w') as jsonfile:
+            #  output the parameters needed to recreate the plane
+            data = { "JuliaPlaneParameters": { "xmin":self.xmin, "xmax":self.xmax, "xlen":self.xlen, "ymin":self.ymin, "ymax":self.ymax, "ylen":self.ylen, "creal":self.c.real, "cimaginary":self.c.imag }}
+
+            #  handle the output of the plane contents
+            mat = self.plane.as_matrix()
+            for row in range( 0, self.xlen ):
+                print( mat[ row ] )
+                l = mat[ row ].tolist()
+                data[ "JuliaPlaneContents" + str( row )] = { self.plane.index[ row ]:  l }
+
+            #  write the accummulated dictionary to the JSON file
+            json.dump(data, jsonfile, indent=4, sort_keys=True, separators=(',', ':'))
+
+            #  we're done, clean up the file
+            jsonfile.close()
+
+
+    def fromJSON( self, filename ):
+        """Read in the contents of a JSON file to rebuild a save julia plane.  We are only interested in
+        the parameters necessary to reconstruct the plane, we do not need to read the contents of the plane
+        itself"""
+        #  open the file for reading
+        from pprint import pprint
+
+        #  open and read in the json file, with help from:  http://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file-in-python
+        with open( filename ) as jsonfile:    
+            data = json.load(jsonfile)
+
+            #  parse the parameter values
+            self.xmin = data["JuliaPlaneParameters"]["xmin"]
+            self.xmax = data["JuliaPlaneParameters"]["xmax"]
+            self.xlen = int( data["JuliaPlaneParameters"]["xlen"] )
+            self.ymin = data["JuliaPlaneParameters"]["ymin"]
+            self.ymax = data["JuliaPlaneParameters"]["ymax"]
+            self.ylen = int( data["JuliaPlaneParameters"]["ylen"] )
+            self.c    = data["JuliaPlaneParameters"]["creal"] + data["JuliaPlaneParameters"]["cimaginary"]*1j
+            self.xstep = (self.xmax - self.xmin)/(self.xlen - 1)
+            self.ystep = (self.ymax - self.ymin)/(self.ylen - 1)
+            self.set_f( self.c )      # set the tranformation function.  Note that this automatically calls refresh()
+
+            #  uncomment this line to see the re-constituted plane
+#            print( self.plane )
+
+            #  we're done, clean up the file
+            jsonfile.close()
+
+
 def julia(c, max=100):
-    """Creates a function f that counts the number of times the operation z = z**2 + c can
-    be done before the magnitude of z exceeds 2. An optional max which defaults to 100
-    provides a limit to the number of loops before a value of 0 is returned.
+    """This method creates and returns a function, f.  The parameters passed to julia are:
+    c - an imagery valued constant that is used in the function f.
+    max - an optional argument that sets the maximum loop count within f.  Default is 100.
+
+    The function f requires a single parameter:
+    z - an imaginary number
+
+    f then performs the operation z = z**2 + c on the z passed to f along with the c value passed to julia.
+    The operation is performed up to max times.
+    The function f returns:
+    1 - if the magnitude of the imagiary number (z) passed in exceeds 2
+    n - count of the number of times the operation can be done *before* the magnitude of z exceeds 2
+    0 - if the max iterations through the loop is reached without the magnitude of z reaching 2
+
+    Note that f's return value of 1 is ambiguous:  it could be because the initial z was too large,
+    or because the operation could be performed once successfully.
     """
     def f(z):
-        n = 0
-        while abs(z)<=2:
-            z = z**2 + c
-            if n >= max:
-                n = 0
-                break
-            n+=1
+        # check to see if the input is already too big
+        if abs( z ) <= 2:
+            n = 0
+            while abs(z)<=2:
+                #  perform the operation
+                z = z**2 + c
+                #  have we exceeded our max loop count-1?
+                if n >= max:
+                    n = 1
+                    break
+                #  count the number of times through the loop
+                n+=1
+            n -= 1  # subtract one to count the total loops *before* exceeding 2, also reports 0 if max loop reached
+        else:
+            #  report input too big
+            n = 1
         return n
+
+    #  return the function pointer to the caller of the julia() method
     return f
 
 
 
 
 
-#  unit testing functions beyond this point
-def test_init_no_params():
-    """Test the creator by passing no parameters.  Since default values are used in place of missing parameters
-       this should *not* cause a TypeError exception"""
-    success = True
-
-    try:
-        testPlane = ComplexPaneNP()
-    except TypeError:
-        """test passes"""
-        success = False
-
-    message = 'Creator should not have generated a TypeError exception, as all unpassed parameters have default values'
-    assert success, message
-
-
-def test_init():
-    """Test the creator by passing the required parameters.  The passed in values should match the object's values"""
-    success = True
-
-    try:
-        xmin = 2
-        xmax = 6
-        ymin = -6
-        ymax = -2
-        testPlane = ComplexPaneNP( xmin, xmax, ymin, ymax )
-
-        #  this line is to force an error to prov the test can fail
-        # xmin = xmin + 1
-
-        #  check that the parameters are all correctly stored
-        if testPlane.xmin != xmin or testPlane.xmax != xmax or testPlane.ymin != ymin or testPlane.ymax != ymax:
-           message = 'Init parameter mismatch: expected %d %d %d %d, actual %d %d %d %d' % (xmin, xmax, ymin, ymax, testPlane.xmin, testPlane.xmax, testPlane.ymin, testPlane.ymax)
-           success = False
-
-    except TypeError:
-        """Test fails, should not have generated an exception"""
-        message = 'Creator generated an exception when correct number of parameters were passed in'
-        success = False
-
-    assert success, message
-
-
-def f2x(x):
-    """function is only used for testing purposes"""
-    return( 2*x )
-
-def itest_setf1():
-    """Test that setting the function to a new function updates the plane with the new transformation values"""
-    #  create a plane
-    tp = ComplexPaneNP( 0, 10, 0, 10 )
-    #  set the function to be f(x) = 2*x
-    tp.set_f( f2x )
-
-    # set up the expected plane
-    xmin = 0
-    xmax = 10
-    xstep = 0.5
-    xlen = 21
-    ymin = 0
-    ymax = 10
-    ystep = 0.5
-    ylen = 21
-    eplane = [[2*(( j*xstep + xmin ) + ( i*ystep + ymin )*1j) for i in range(ylen)] for j in range(xlen)]
-
-    #  this line is to force an error to prove the test can fail
-    #eplane[1][1] = 5
-
-    #  do the expected and actual planes match?
-    success = tp.plane == eplane
-    message = 'set_f() did not correctly transform the plane to double the coordinate values'
-    assert success, message
-
-
-
-def test_setf2():
-    """Set the transformation function to be something other than a function(), which should fail,
-    meaning the test was successful"""
-    #  create a plane
-    tp = ComplexPaneNP( 1, 10, 1, 10 )
-
-    try:
-        #  set the function to be f(x) = 2*x
-        tp.set_f( tp )
-        message = 'Test Failed, succeeded in setting function to a non-function value'
-        success = False
-    except TypeError:
-        """Test succeeds, exception generated"""
-        success = True
-
-    assert success, message
-
-def itest_zoom1():
-    """Test the zoom function with valid values.  Zoom should move/reset the 2D plane to a known configuration"""
-    # set up the expected plane
-    xmin = 0
-    xmax = 10
-    xstep = 0.5
-    xlen = 21
-    ymin = 0
-    ymax = 10
-    ystep = 0.5
-    ylen = 21
-    eplane = [[(( j*xstep + xmin ) + ( i*ystep + ymin )*1j) for i in range(ylen)] for j in range(xlen)]
-
-    #  create a plane
-    tp = ComplexPaneNP( 100, 200, -100, 0 )
-    tp.zoom(xmin, xmax, ymin, ymax)
-
-    #  do the expected and actual planes match?
-    success = tp.plane == eplane
-    message = 'zoom() did not correctly transform the plane to the new coordinate values'
-    assert success, message
-
-def test_zoom2():
-    """Test the zoom function with invalid values.  Zoom should generate an exception"""
-    #  create a plane
-    tp = ComplexPaneNP( 100, 200, -100, 0 )
-    try:
-        tp.zoom( "one", 100, -1, 3)
-        message = 'Test Failed, zoom did not catch use of an invalid parameter'
-        success = False
-    except TypeError:
-        """Test succeeds, exception generated"""
-        success = True
-
-    assert success, message
-
-
-
-def itest_refresh1():
-    """Test the refresh function.  Create a plane, corrupt the data in the plane, refresh and verify the data is once again correct"""
-    #  create a plane
-    tp = ComplexPaneNP( 100, 200, -100, 0 )
-    #  create a duplicate plane
-    ep = ComplexPaneNP( 100, 200, -100, 0 )
-
-    #  corrupt the original test plane
-    tp.plane = [[(-1 +  -1j) for i in range(tp.ylen)] for j in range(tp.xlen)]
-
-    # refresh the plane
-    tp.refresh()
-
-    #  this line is to force an error to prove the test can fail
-    #ep.plane[1][1] = -1
-
-    #  do the expected and actual planes match?
-    success = tp.plane == ep.plane
-    message = 'refresh() did not correctly retore the plane to the expected coordinate values'
-    assert success, message
+#  unit testing functions are in the file test_cplane_np.py
 
